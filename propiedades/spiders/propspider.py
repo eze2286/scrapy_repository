c8e32665c3ff01ae26dc9d6c8829008953fee88a
Ezequiel <69882938+eze2286@users.noreply.github.com>
2023-10-13 20:06:08 -0300

    Update propspider.py

--- propiedades/spiders/propspider.py
+++ propiedades/spiders/propspider.py
@@ -1,7 +1,7 @@
 import scrapy
 from scrapy.loader import ItemLoader
-from propiedades.items import PropiedadesItem
 from scrapy.loader.processors import Join, MapCompose
+from propiedades.items import PropiedadesItem
 
 
 class PropspiderSpider(scrapy.Spider):
