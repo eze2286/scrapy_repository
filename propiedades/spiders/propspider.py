056e512a8a53ccb76223dfe5edfdf69c0ed35089
Ezequiel <69882938+eze2286@users.noreply.github.com>
2023-10-14 16:31:34 -0300

    Update propspider.py

--- propiedades/spiders/propspider.py
+++ propiedades/spiders/propspider.py
@@ -13,6 +13,16 @@ class PropspiderSpider(scrapy.Spider):
     def __init__(self, *args ,**kwargs):
         super(PropspiderSpider, self).__init__(*args, **kwargs)
         self.pages_to_scrape = int(kwargs.get('pages', -1))
+        
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                meta={
+                    "proxy": "http://api.scraperapi.com?api_key=f851980e1f42ff49ada8556f1c94c3a3"
+                }
+            )
 
     def parse(self, response, **kargs):
         #contenedor_links = response.xpath('//div[@class="resultadoContenedorDatosResultados"]//a/@href')
