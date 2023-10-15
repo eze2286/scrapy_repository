5cd549f5ca2876c106ee6778625a97f5a4626cfb
Ezequiel <69882938+eze2286@users.noreply.github.com>
2023-10-15 00:36:12 -0300

    Update propspider.py

--- propiedades/spiders/propspider.py
+++ propiedades/spiders/propspider.py
@@ -33,7 +33,7 @@ class PropspiderSpider(scrapy.Spider):
             print("Pages to scrape: ", self.pages_to_scrape)
             #print(url)         
             yield scrapy.Request(url=url, callback=self.parse_prop, meta={"precio": precio, "codigo": codigo, "direccion": direccion,
-                                                                         "proxy": "http://scraperapi:f851980e1f42ff49ada8556f1c94c3a3@proxy-server.scraperapi.com:8001"})
+                                                                         })
         
         try:        
             next_page = response.xpath('//a/font[contains(text(), "Siguiente")]/../@href')[0]
